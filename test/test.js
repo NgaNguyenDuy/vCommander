7be905bb1ce731bcae2a312c6161ee449e93e04e
NgaNguyenDuy <kyo1508@gmail.com>
2014-09-24 05:51:34 +0700

    testing something

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ describe('Array test', function() {
     describe('indexOf()', function() {
         it('should return -1 when the value is not present', function(done) {
             assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+            done();
         });
     });
 });
